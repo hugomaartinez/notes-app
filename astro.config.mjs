cfc943f978fb1a29af77e46c44affc6e50b4eedb
Hugo Martínez Barros <hugo.martinez.ba2003@gmail.com>
2025-01-21 00:09:55 +0100

    Agregar configuración de salida del servidor en astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,7 @@ import vercel from '@astrojs/vercel';
 
 // https://astro.build/config
 export default defineConfig({
+  output: 'server',
   integrations: [tailwind(), auth()],
-  adapter: vercel()
+  adapter: vercel(),
 });
